8f2131eb25279f3204d02aeb1763e621a5d1c860
Mintu Kumar <mintu.kumar@cern.ch>
2021-12-07 15:57:36 +0530

    gen info crab not working in ui2

--- crab/Gen_Study_Sebastien/crab_cfg_skimTree.py
+++ crab/Gen_Study_Sebastien/crab_cfg_skimTree.py
@@ -5,7 +5,7 @@ config = Configuration()
 
 config.section_("General")
 
-config.General.requestName = 'Tbarchannel_wtop1p3_Tree_2016'
+config.General.requestName = 'TTTo2L2Nu_widthx1p3_Tree_2016'
 
 config.General.transferLogs=True
 config.section_("JobType")
@@ -18,7 +18,7 @@ config.JobType.inputFiles = ['crab_script_skimTree.py','Gen_mass_functions.py','
 config.JobType.sendPythonFolder	 = True
 config.section_("Data")
 
-config.Data.inputDataset = '/ST_t-channel_antitop_4f_InclusiveDecays_wtop1p3_TuneCP5_13TeV-powheg-madspin-pythia8/RunIISummer20UL16NanoAODv2-106X_mcRun2_asymptotic_v15-v1/NANOAODSIM'
+config.Data.inputDataset = '/TTTo2L2Nu_widthx1p3_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv2-106X_mcRun2_asymptotic_v15-v1/NANOAODSIM'
 
 #config.Data.inputDBS = 'phys03'
 config.Data.inputDBS = 'global'
@@ -30,11 +30,11 @@ config.Data.unitsPerJob = 1
 #config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt" 
 #config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Final/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt"
 
-config.Data.outLFNDirBase = '/store/user/mikumar/RUN2/Gen_topwidthV2/SIXTEEN/MC/Tbarchannel_wtop1p3'
+config.Data.outLFNDirBase = '/store/user/mikumar/RUN2/Gen_topwidthTTbarULV9/SIXTEEN/MC/TTTo2L2Nu_widthx1p3'
 
 config.Data.publication = False
 
-config.Data.outputDatasetTag = 'Tree_17_Nov21_MC2016_Tbarchannel_wtop1p3'
+config.Data.outputDatasetTag = 'Tree_07_Dec21_MC2016_TTTo2L2Nu_widthx1p3'
 
 config.section_("Site")
 config.Site.storageSite = "T2_IN_TIFR"
