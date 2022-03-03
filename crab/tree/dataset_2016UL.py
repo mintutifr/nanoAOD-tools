325907f5984f652e5058d87bf24e49ea6240a038
Mintu Kumar <mintu.kumar@cern.ch>
2022-03-03 12:15:07 +0530

    ttbar width sample updated

--- crab/tree/dataset_2016UL.py
+++ crab/tree/dataset_2016UL.py
@@ -82,10 +82,13 @@ Datasets_Alt_MC_2016 =[
                 #'/TTToSemiLeptonic_wtop1p15_TuneCP5_13TeV-powheg-pythia8/',
                 #'/TTToSemiLeptonic_wtop1p3_TuneCP5_13TeV-powheg-pythia8/',
 
+
+		'/TTTo2L2Nu_widthx0p55_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx0p7_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx0p85_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx1p15_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx1p3_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv2-106X_mcRun2_asymptotic_v15-v1/NANOAODSIM',
+		'/TTTo2L2Nu_widthx1p45_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM'
 ]
 
 RequestName_Alt_MC_2016 = [
--- crab/tree/dataset_2017UL.py
+++ crab/tree/dataset_2017UL.py
@@ -92,11 +92,12 @@ datasets_Alt_MC_2017 =[
                 #'/TTToSemiLeptonic_wtop1p15_TuneCP5_13TeV-powheg-pythia8/',
                 #'/TTToSemiLeptonic_wtop1p3_TuneCP5_13TeV-powheg-pythia8/',	
 
+		'/TTTo2L2Nu_widthx0p55_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx0p7_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx0p85_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx1p15_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM',
 		'/TTTo2L2Nu_widthx1p3_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM',
-
+		'/TTTo2L2Nu_widthx1p45_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM'
 		
 ]
 
