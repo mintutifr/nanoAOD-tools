d59234029225ac33f5906d2882abfe9ffdeaf083
Mintu Kumar <mintu.kumar@cern.ch>
2022-03-10 14:40:55 +0530

    btv update

--- crab/tree/btv.py
+++ crab/tree/btv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import *
 
-btagSF2016 = lambda: btagSFProducer("2016",'deepcsv', ['L','M','T','shape_corr'])
-btagSF2017 = lambda: btagSFProducer("2017",'deepcsv', ['L','M','T','shape_corr'])
+btagSF2016preVFP = lambda: btagSFProducer("UL2016preVFP",'deepjet', ['shape_corr'])
+btagSF2016postVFP = lambda: btagSFProducer("UL2016postVFP",'deepjet', ['shape_corr'])
+btagSF2017 = lambda: btagSFProducer("UL2017",'deepjet', ['L','M','T','shape_corr'])
+btagSF2018 = lambda: btagSFProducer("UL2018",'deepjet', ['L','M','T','shape_corr'])
 
