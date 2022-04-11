9d1e4c9d772bd8e42a7311d848f999bda3f91ea1
Mintu Kumar <mintu.kumar@cern.ch>
2022-04-11 07:29:46 +0200

    btv module modified acc UL

--- crab/tree/btv.py
+++ crab/tree/btv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import *
 
-btagSF2016preVFP = lambda: btagSFProducer("UL2016preVFP",'deepjet', ['shape_corr'])
-btagSF2016postVFP = lambda: btagSFProducer("UL2016postVFP",'deepjet', ['shape_corr'])
-btagSF2017 = lambda: btagSFProducer("UL2017",'deepjet', ['L','M','T','shape_corr']) #remeber for fix working point to evelavate weights for L, M, T there are saprate input file acc. to new recomadation which not in place in btv producer
-btagSF2018 = lambda: btagSFProducer("UL2018",'deepjet', ['L','M','T','shape_corr']) #remeber for fix working point to evelavate weights for L, M, T there are saprate input file acc. to new recomadation which not in place in btv producer
+btagSFUL2016preVFP = lambda: btagSFProducer("UL2016preVFP",'deepjet', ['shape_corr'])
+btagSFUL2016postVFP = lambda: btagSFProducer("UL2016postVFP",'deepjet', ['shape_corr'])
+btagSFUL2017 = lambda: btagSFProducer("UL2017",'deepjet', ['shape_corr']) #remeber for fix working point to evelavate weights for L, M, T there are saprate input file acc. to new recomadation which not in place in btv producer
+btagSFUL2018 = lambda: btagSFProducer("UL2018",'deepjet', ['shape_corr']) #remeber for fix working point to evelavate weights for L, M, T there are saprate input file acc. to new recomadation which not in place in btv producer
 
