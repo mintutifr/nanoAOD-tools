297e621f8fe2fd32c8c5419986806ab45dc3717b
Mintu Kumar <mintu.kumar@cern.ch>
2022-04-11 13:05:27 +0200

    add files for fermilab env

--- crab/tree/crab_cfg_skimTree.py
+++ crab/tree/crab_cfg_skimTree.py
@@ -5,36 +5,35 @@ config = Configuration()
 
 config.section_("General")
 
-config.General.requestName = 'Run2016H_el_Tree'
+config.General.requestName = 'WJetsToLNu_2J_Tree_UL2017'
 
 config.General.transferLogs=True
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
 config.JobType.scriptExe = 'crab_script_skimTree.sh'
-config.JobType.inputFiles = ['ElectronSF','MuonSF','crab_script_skimTree.py','scaleFactor.py','btv.py','jme.py','../../scripts/haddnano.py','keep_and_drop.txt','MainModule.py','Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt','Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt']
+config.JobType.inputFiles = ['crab_script_skimTree.py','../../scripts/haddnano.py','keep_and_drop.txt','MainModule.py','btv.py']
 #hadd nano will not be needed once nano tools are in cmssw
 #config.JobType.outputFiles = ['B_DeepCSV_Efficiency.root']
 config.JobType.sendPythonFolder=True
 config.section_("Data")
 
-config.Data.inputDataset = '/SingleElectron/Run2016H-02Apr2020-v1/NANOAOD'
+config.Data.inputDataset = '/WJetsToLNu_2J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM'
 
 #config.Data.inputDBS = 'phys03'
 config.Data.inputDBS = 'global'
-#config.Data.splitting = 'FileBased'
+config.Data.splitting = 'FileBased'
 #config.Data.splitting = 'Automatic'
-config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 150
-#config.Data.totalUnits = 1
-config.Data.lumiMask = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
-#config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt" 
+#config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = 10
+#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'
 
-config.Data.outLFNDirBase = '/store/user/mikumar/RUN2/Tree_crab/SIXTEEN/Data_el_new/Run2016H_el'
+config.Data.outLFNDirBase = '/store/user/mikumar/RUN2_UL/Tree_crab/SEVENTEEN/MC/check/WJetsToLNu_2J'
 
 config.Data.publication = True
 
-config.Data.outputDatasetTag = 'Tree_20_Jan22_Run2016H_el'
+config.Data.outputDatasetTag = 'Tree_09_Apr22_MCUL2017_WJetsToLNu_2J_check'
 
 config.section_("Site")
 config.Site.storageSite = "T2_IN_TIFR"
--- crab/tree/crab_script_skimTree.py
+++ crab/tree/crab_script_skimTree.py
@@ -6,21 +6,22 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 #from exampleModule import *
 from MainModule import *
-#from jme import *
+from btv import *
 #from EfficiencyModule import *
-from  btv import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
 #from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr16 import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import *
 
-treecut = "nJet>0 && Jet_pt>20 && (Sum$(Muon_pt>20)>0 || Sum$(Electron_pt>30)>0) && Entry$<10000"#(nMuon>0 || nElectron>0)" # && (Muon_pt>5 || Electron_pt>10) (nMuon>0 || nElectron>0)"
-#inputFiles=["root://cms-xrd-global.cern.ch//store/data/Run2017B/SingleMuon/NANOAOD/02Apr2020-v1/50000/FBFADBAD-5164-6A4D-A19E-EFF003090EC5.root"]
+treecut = "nJet>0 && Jet_pt>20 && (Sum$(Muon_pt>20)>0 || Sum$(Electron_pt>30)>0)"# && Entry$<10000"#(nMuon>0 || nElectron>0)" # && (Muon_pt>5 || Electron_pt>10) (nMuon>0 || nElectron>0)"
+inputFiles=["root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL16NanoAODAPVv9/ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mcRun2_asymptotic_preVFP_v11-v1/280000/D2AF2E22-BD5B-B542-B6BA-6ABF26FAC0FC.root"]
+#/store/data/Run2017B/SingleMuon/NANOAOD/02Apr2020-v1/50000/FBFADBAD-5164-6A4D-A19E-EFF003090EC5.root"]
+#inputFiles=["/afs/cern.ch/user/m/mikumar/work/private/NanoAOD_new/CMSSW_10_6_0/src/Inputroot_files/Tree/single_electrondata_2016_runD/35F06149-6032-374D-8750-10CAAF330EEE.root"]
 #inputFiles=["/afs/cern.ch/user/m/mikumar/work/private/NanoAOD_new/CMSSW_10_6_0/src/Inputroot_files/Tree/single_muondata_2016_runD/5A4AA866-8941-334C-A591-7D2728C6F63F.root"]
-inputFiles=["/afs/cern.ch/user/m/mikumar/work/private/NanoAOD_new/CMSSW_10_6_0/src/Inputroot_files/Tree/ST_tch/06314878-0D6B-544B-9E2C-C5EDCD0666D3_2017.root"]
+#inputFiles=["/afs/cern.ch/user/m/mikumar/work/private/NanoAOD_new/CMSSW_10_6_0/src/Inputroot_files/Tree/ST_tch/06314878-0D6B-544B-9E2C-C5EDCD0666D3_2017.root"]
 p=PostProcessor(".",
 		inputFiles,
 		treecut,
-		modules=[MainModuleConstr_mc_2016(),btagSF2016preVFP(),puWeight_UL2016()],
+		modules=[MainModuleConstr_mc_UL2017()],#btagSFUL2017(),puWeight_UL2017()],
 		outputbranchsel="keep_and_drop.txt",
 		provenance=True,
 		fwkJobReport=True,
