48efc063cb4aa46ca7bf92570a4ca356acc4dfb4
Mintu Kumar <mintu.kumar@cern.ch>
2022-04-13 16:24:36 +0200

    update minitree crab sub code

---
+++ crab/minitree/dataset_UL2016postVFP_phy3.py
@@ -0,0 +1,9 @@
+Datasets_SingleElectron_data_UL2016 = {
+		'Run2017B_el' : '/SingleElectron/mikumar-Tree_11_Nov20_Run2017B_el-b3a1988b93a84bf68fff9ff80c388e61/USER',
+		'Run2017C_el' :  '/SingleElectron/mikumar-Tree_11_Nov20_Run2017C_el-b3a1988b93a84bf68fff9ff80c388e61/USER',
+}
+
+Datasets_SingleMuon_data_UL2016 = {
+		'Run2017B_mu' : '/SingleMuon/mikumar-Tree_october_Seventeen_Run2017B_mu-b3a1988b93a84bf68fff9ff80c388e61/USER',
+		'Run2017C_mu' : '/SingleMuon/mikumar-Tree_october_Seventeen_Run2017C_mu-b3a1988b93a84bf68fff9ff80c388e61/USER',
+}
