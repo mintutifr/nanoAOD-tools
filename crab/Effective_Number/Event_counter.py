04bd3f20763d8f0a0befc9a8828f6eb6b656b559
Mintu Kumar <mintu.kumar@cern.ch>
2022-05-19 10:13:44 +0200

    cal Event preVFP 2016

---
+++ crab/Effective_Number/Event_counter.py
@@ -0,0 +1,58 @@
+import fileinput, string, sys, os, time, datetime,subprocess
+import argparse as arg
+sys.path.append('../tree')
+parser = arg.ArgumentParser(description='inputs discription')
+parser.add_argument('-y', '--year', dest='inputs', type=str, nargs=1, help="Year [ UL2016preVFP , UL2016postVFP , UL2017 , UL2018 ]")
+args = parser.parse_args()
+
+if args.inputs == None:
+        print "USAGE: %s [-h] [-y <Data year>]"%(sys.argv [0])
+        sys.exit (1)
+
+if args.inputs[0] not in [ 'UL2016preVFP' , 'UL2016postVFP' , 'UL2017' , 'UL2018']:
+    print('Error: Incorrect choice of year, use -h for help')
+    exit()
+
+print "year = ",args.inputs[0]
+year   = args.inputs[0]
+date   = datetime.datetime.now()
+
+
+import ROOT as R
+R.gInterpreter.ProcessLine('#include "LHEWeightSign.C"')
+
+if(year == 'UL2016preVFP'):
+    from dataset_UL2016preVFP import *
+    Datasets = Datasets_MC_UL2016APV
+if(year == 'UL2016postVFP'):
+    from dataset_UL2016postVFP import *
+    Datasets = Datasets_MC_UL2016
+
+if(year == 'UL2017'):
+    from dataset_UL2017 import *
+    Datasets = Datasets_MC_UL2017
+
+RequestNames = Datasets.keys()
+print RequestNames
+print "len(Datasets) = ",len(Datasets)
+
+for i in range(0,len(RequestNames)):
+    RequestName = RequestNames[i]
+    Dataset = Datasets[RequestName]
+    print
+    print "---------------------------------------     ", RequestName ,"      ---------------------------------"
+    cmd_count = 'dasgoclient --query="file, dataset='+Dataset+' | sum(file.nevents)"'
+    #print cmd_count
+    os.system(cmd_count)
+    p = subprocess.Popen(cmd_count, stdout=subprocess.PIPE, shell=True)
+    (output, err) = p.communicate()
+    #p_status = p.wait()
+
+    print " Number of Events : ",output[19:] 
+   
+    if(not(RequestName.find("QCD")!=-1)): 
+    	cmd_dasgoclint = 'dasgoclient -query="file dataset='+Dataset+'" > filename.txt'
+    	#print cmd_dasgoclint
+    	os.system(cmd_dasgoclint) 
+  
+    	R.LHEWeightSign() 
--- crab/Effective_Number/LHEWeightSign.C
+++ crab/Effective_Number/LHEWeightSign.C
@@ -13,25 +13,27 @@ void LHEWeightSign(){
      string fileName;
      ifstream infile;
      infile.open("filename.txt");
-     TH1F *h = new TH1F("h","h",3,-1.5,1.5);h->Sumw2();
+     TH1F *h = new TH1F("h","h",2,-5,5);h->Sumw2();
      //TH1F* h; 
      ULong64_t count=0;
      while(!infile.eof()){
           getline(infile,fileName);
 	  TString file = "root://cms-xrd-global.cern.ch//"+fileName;
-	  cout<<file<<endl;
+	  //cout<<file<<endl;
 	  if(file=="root://cms-xrd-global.cern.ch//") continue;
           TFile *f = TFile::Open(file);
           TTree *tree;
           tree = (TTree*)gDirectory->Get("Events");
-	  TH1F *temp = new TH1F("temp","temp",3,-1.5,1.5);temp->Sumw2(); temp->Reset();
+	  TH1F *temp = new TH1F("temp","temp",2,-5,5);temp->Sumw2(); temp->Reset();
    	  tree->Project("temp","LHEWeight_originalXWGTUP/abs(LHEWeight_originalXWGTUP)");
 	  h->Add(temp);
 	  delete temp; delete tree; delete f;
 	  count++;
 	  if(count%100==0) std::cout<<"# of files read = "<<count<<std::endl;
      }
-	  UInt_t EffectiveNo = h->GetBinContent(3)-h->GetBinContent(1);
-	  cout<<"Effective No ="<<EffectiveNo<<endl;
-	  cout<<"totle files = "<<count<<endl;
+     UInt_t EffectiveNo = h->GetBinContent(2)-h->GetBinContent(1);
+     cout<<"Effective No. Events ="<<EffectiveNo<<endl;
+     cout<<"totle files Evaluated = "<<count<<endl;
+     delete h; 
+	
 }
