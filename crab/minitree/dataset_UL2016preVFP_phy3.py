e49e00836b4c8870aa8680ab074b47dbb323720e
Mintu Kumar <mintu.kumar@cern.ch>
2022-05-29 13:51:56 +0530

    add new datasets

--- crab/minitree/dataset_UL2016preVFP_phy3.py
+++ crab/minitree/dataset_UL2016preVFP_phy3.py
@@ -43,18 +43,18 @@ Datasets_MC_UL2016APV = {
 }
 
 Datasets_SingleMuon_data_UL2016APV = {
-		'Run2016B_preVFP_mu' : '/SingleMuon/mikumar-Tree_20_Apr22_Run2016B-ver2_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016C_preVFP_mu' : '/SingleMuon/mikumar-Tree_20_Apr22_Run2016C-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016D_preVFP_mu' : '/SingleMuon/mikumar-Tree_20_Apr22_Run2016D-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016E_preVFP_mu' : '/SingleMuon/mikumar-Tree_20_Apr22_Run2016E-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016F_preVFP_mu' : '/SingleMuon/mikumar-Tree_20_Apr22_Run2016F-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016B_preVFP_mu' : '/SingleMuon/mikumar-Tree_17_May22_Run2016B-ver2_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016C_preVFP_mu' : '/SingleMuon/mikumar-Tree_17_May22_Run2016C-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016D_preVFP_mu' : '/SingleMuon/mikumar-Tree_17_May22_Run2016D-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016E_preVFP_mu' : '/SingleMuon/mikumar-Tree_17_May22_Run2016E-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016F_preVFP_mu' : '/SingleMuon/mikumar-Tree_17_May22_Run2016F-HIPM_mu-11cca84e9421f041a8fcb7fd41edd277/USER',
 }
 
-Datasets_SingleElectron_data_UL2016APV= {
-		'Run2016B_preVFP_el' : '/SingleElectron/mikumar-Tree_20_Apr22_Run2016B-ver2_el-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016C_preVFP_el' : '/SingleElectron/mikumar-Tree_20_Apr22_Run2016C-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016D_preVFP_el' : '/SingleElectron/mikumar-Tree_20_Apr22_Run2016D-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016E_preVFP_el' : '/SingleElectron/mikumar-Tree_20_Apr22_Run2016E-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
-		'Run2016F_preVFP_el' : '/SingleElectron/mikumar-Tree_20_Apr22_Run2016F-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
+Datasets_SingleElectron_data_UL2016APV = {
+		'Run2016B_preVFP_el' : '/SingleElectron/mikumar-Tree_17_May22_Run2016B-ver2_el-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016C_preVFP_el' : '/SingleElectron/mikumar-Tree_17_May22_Run2016C-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016D_preVFP_el' : '/SingleElectron/mikumar-Tree_17_May22_Run2016D-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016E_preVFP_el' : '/SingleElectron/mikumar-Tree_17_May22_Run2016E-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
+		'Run2016F_preVFP_el' : '/SingleElectron/mikumar-Tree_17_May22_Run2016F-HIPM_el-11cca84e9421f041a8fcb7fd41edd277/USER',
 
 }
