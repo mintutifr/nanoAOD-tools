1a88249091e1e2bbfcadd7534133a2ed82b93417
Mintu Kumar <mintu.kumar@cern.ch>
2022-06-01 11:43:41 +0530

    some fixes and debugs

--- crab/minitree/crab_script_Minitree.py
+++ crab/minitree/crab_script_Minitree.py
@@ -14,9 +14,9 @@ treecut = cut_2J1T1_mu_2016
 
 #inputFiles=["6E1B25E9-BBAE-B14F-92C1-FDC95C9EC4A2_Skim.root"]
 #inputFiles=["root://se01.indiacms.res.in//store/user/mikumar/RUN2/Tree_crab/SIXTEEN/Data_mu_new/Run2016D_mu/SingleMuon/Tree_12_Oct21_Run2016D_mu/211012_170845/0000/tree_1.root"]
-inputFiles=["/afs/cern.ch/user/m/mikumar/work/private/NanoAOD_new/CMSSW_10_6_24/src/Inputfiles/skimtree/DATA/Single_muon/RUNB/tree_10.root"]
+inputFiles=['root://se01.indiacms.res.in//store/user/mikumar/RUN2_UL/Tree_crab/SIXTEEN/MC_preVFP/ttbar_FullyLeptonic/TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/Tree_22_Apr22_MCUL2016preVFP_ttbar_FullyLeptonic_check/220422_171036/0000/tree_31.root']
 p=PostProcessor(".",
-		inputFiles(),
+		inputFiles,
 		treecut,
 		modules=[MinitreeModuleConstr2J1T1_mu_data_UL2016preVFP(), jmeCorrectionsULRun2016C_preVFP_DATA_AK4CHS()],
 		outputbranchsel="keep_and_drop_mu_Minitree.txt",
