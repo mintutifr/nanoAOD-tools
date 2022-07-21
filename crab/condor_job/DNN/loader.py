a0a955d2fba9532bb7dea395bf8012f9b14ea53e
Mintu Kumar <mintu.kumar@cern.ch>
2022-07-22 03:00:24 +0530

    add SB data as QCD

--- crab/condor_job/DNN/loader.py
+++ crab/condor_job/DNN/loader.py
@@ -8,7 +8,7 @@ import sys
 import os
 import root_numpy
 
-dir16 = '/grid_mnt/t3storage3/mikumar/UL_Run2/SIXTEEN_preVFP/minitree/Mc/2J1T1/'
+dir16 = '/grid_mnt/t3storage3/mikumar/UL_Run2/SIXTEEN_preVFP/minitree/Mc/'
 dir17 = ''
 dir18 = ''
 
@@ -23,26 +23,52 @@ _branches_mu = [
         'dR_bJet_lJet',
         'FW1',
         'Jet_pt',
-        'Jet_btagDeepFlavB',
         'Jet_partonFlavour',
         'nbjet_sel',
-        'bJethadronFlavour',
         'MuonCharge',
+        'bJetdeepJet',
+        'event'
+    ]
+_branches_mu_Data = [
+        'MuonEta',
+        'dEta_mu_bJet',
+        'mtwMass',
+        'abs_lJetEta',
+        'jetpTSum',
+        'diJetMass',
+        'cosThetaStar',
+        'dR_bJet_lJet',
+        'FW1',
+        'Jet_pt',
+        'nbjet_sel',
+        'MuonCharge',
+        'bJetdeepJet',
         'event'
     ]
 def load_dataset ( max_entries = -1, channel = "Tchannel", lep = "mu" ):
     chain = ROOT.TChain('Events')
-    chain.Add(dir16+'Minitree_'+channel+'_2J1T1_'+lep+'.root')
+    if "Data" in channel:
+        chain.Add(dir16+'2J1T0/Minitree_'+channel+'_2J1T0_'+lep+'.root')
+        print 'Entries Data: ',chain.GetEntries(), "\tSelected : ",max_entries
 
-    print 'Entries : ',chain.GetEntries(), "\tSelected : ",max_entries
+        _dataset = root_numpy.tree2array (chain,
+                branches = _branches_mu_Data,
+                selection = '',
+                stop = max_entries
+                )
+        return { b : _dataset[b] for b in _branches_mu_Data }
+    else:
+        chain.Add(dir16+'2J1T1/Minitree_'+channel+'_2J1T1_'+lep+'.root')
 
-    _dataset = root_numpy.tree2array (chain,
-        branches = _branches_mu,
-        selection = '',
-        stop = max_entries
-        )
+        print 'Entries : ',chain.GetEntries(), "\tSelected : ",max_entries
 
-    return { b : _dataset[b] for b in _branches_mu }
+        _dataset = root_numpy.tree2array (chain,
+                branches = _branches_mu,
+                selection = '',
+                stop = max_entries
+                )
+
+        return { b : _dataset[b] for b in _branches_mu }
 
 
 if __name__ == '__main__':
--- crab/condor_job/DNN/save_dataframe.py
+++ crab/condor_job/DNN/save_dataframe.py
@@ -8,45 +8,44 @@ import root_numpy
 import sys
 from IPython.display import display
 
-channels = ['Tchannel', 'Tbarchannel','tw_top', 'tw_antitop', 'Schannel','ttbar_SemiLeptonic','ttbar_FullyLeptonic', 'WJetsToLNu_0J', 'WJetsToLNu_1J', 'WJetsToLNu_2J', 'DYJets', 'WWTolnulnu', 'WZTo2Q2L', 'ZZTo2Q2L', 'QCD']
+channels = ['Tchannel', 'Tbarchannel','tw_top', 'tw_antitop', 'Schannel','ttbar_SemiLeptonic','ttbar_FullyLeptonic', 'WJetsToLNu_0J', 'WJetsToLNu_1J', 'WJetsToLNu_2J', 'DYJets', 'WWTolnulnu', 'WZTo2Q2L', 'ZZTo2Q2L', 'DataULpreVFP2016']
 
 datasets = []
 for channel in channels:
-        datasets.append(load_dataset(-1,channel, "mu"))
+        datasets.append(load_dataset(100,channel, "mu")) #apped all data frames in datasets
 
-bjet_deepjet_score = []
+#bjet_deepjet_score = [] 
 corr_assig = []
-for channel_no in range(0,len(channels)):
-        corr_assig_channel = []
-        bjet_deepjet_score_channel = []
-        Jet_btagDeepFlavB_channel = datasets[channel_no]["Jet_btagDeepFlavB"]
-        Jet_partonFlavour_channel = datasets[channel_no]["Jet_partonFlavour"]
+for channel_no in range(0,len(channels)-1): 
+        assig_channel = []
+        #bjet_deepjet_score_channel = []
+        #Jet_btagDeepFlavB_channel = datasets[channel_no]["Jet_btagDeepFlavB"] # get Jet_btagDeepFlavB from the list of dataframes`
+        Jet_partonFlavour_channel = datasets[channel_no]["Jet_partonFlavour"]  # get Jet_partonFlavour from the list of dataframes
         nbjet_sel_channel = datasets[channel_no]["nbjet_sel"]
-        bJethadronFlavour_channel = datasets[channel_no]["bJethadronFlavour"] 
-        lepton_channel = datasets[channel_no]["MuonCharge"]
+        #bJethadronFlavour_channel = datasets[channel_no]["bJethadronFlavour"] 
+        lepton_charge_channel = datasets[channel_no]["MuonCharge"]
         #Event_channel = datasets[channel_no]["event"]
-        for i in range (0,len(datasets[channel_no]["MuonEta"])):
-                if not len(Jet_btagDeepFlavB_channel[i]) == 0:
-                        bjet_deepjet_score_channel.append(Jet_btagDeepFlavB_channel[i][nbjet_sel_channel[i]])
-                        if(Jet_partonFlavour_channel[i][nbjet_sel_channel[i]]*lepton_channel[i]==5):
-                                corr_assig_channel.append(1) 
+        for i in range (0,len(datasets[channel_no]["MuonEta"])): #loop over all events per channel since outer loop is over channels
+                #if not len(Jet_btagDeepFlavB_channel[i]) == 0:   # this should be alwayes true since we always have one tight b Jet
+                        #bjet_deepjet_score_channel.append(Jet_btagDeepFlavB_channel[i][nbjet_sel_channel[i]])
+                        if(Jet_partonFlavour_channel[i][nbjet_sel_channel[i]]*lepton_charge_channel[i]==5):
+                                assig_channel.append(1) # this need not be done for each channel we suppose to do it for only Tchannel and Tbarchannel 
                         else:
-                                corr_assig_channel.append(0)
-                                #print Event_channel[i]," : ",bJethadronFlavour_channel[i]," : ",lepton_channel[i]
-                else:
-                        bjet_deepjet_score_channel.append(-1)
-        bjet_deepjet_score.append(bjet_deepjet_score_channel)
-        corr_assig.append(corr_assig_channel)
+                                assig_channel.append(0)
+                                #print Event_channel[i]," : ",bJethadronFlavour_channel[i]," : ",lepton_charge_channel[i]
+                #else:
+                #        bjet_deepjet_score_channel.append(-1)
+        #bjet_deepjet_score.append(bjet_deepjet_score_channel)
+        corr_assig.append(assig_channel)
         del Jet_partonFlavour_channel
-        del corr_assig_channel
-        del bjet_deepjet_score_channel
-        del Jet_btagDeepFlavB_channel
+        del assig_channel
+        #del bjet_deepjet_score_channel
+        #del Jet_btagDeepFlavB_channel
         del nbjet_sel_channel
-        del bJethadronFlavour_channel
-        del lepton_channel
+        #del bJethadronFlavour_channel
+        del lepton_charge_channel
         
 
-    
 
 VARS = ['MuonEta',
         'dEta_mu_bJet',
@@ -57,19 +56,24 @@ VARS = ['MuonEta',
         'cosThetaStar',
         'dR_bJet_lJet',
         'FW1',
+        'bJetdeepJet'
         ]
 
-df = {}
+df = {}  # remember this will be list of dataframe
 
 channel_no=0
-for channel in channels:
-        df[channel] = pd.DataFrame(datasets[channel_no],columns=VARS)
-        df[channel]['deepJet_score_b'] = np.asarray(bjet_deepjet_score[channel_no])
-        df[channel]['Assig'] = np.asarray(corr_assig[channel_no])
-        channel_no=channel_no+1
+for channel in channels: #try to enumare funchtion to remove channel_no in this loopi
+     
+     df[channel] = pd.DataFrame(datasets[channel_no],columns=VARS) #adding dataframes in the list
+     #df[channel]['deepJet_score_b'] = np.asarray(bjet_deepjet_score[channel_no])
+     #if( ('Tchannel' in channel) or  ('Tbarchannel' in channel)):
+     if("Data" not in channel):
+        df[channel]['Assig'] = np.asarray(corr_assig[channel_no]) # adding new variable in each dataframe of a given channel 
+     channel_no=channel_no+1
+
 del datasets
-del bjet_deepjet_score
-del corr_assig
+#del bjet_deepjet_score
+del corr_assig #this has been added
 
 #defin dec to store info channel wise
 df_train = {}
@@ -79,9 +83,9 @@ df_test = {}
 #loop to devide sample in train, valid and test
 for channel in channels:
         Entries = df[channel].shape[0]
-        df_train[channel] = df[channel].iloc[:int(Entries/2),:]
-        df_valid[channel] = df[channel].iloc[int(Entries/2):int(3*Entries/4),:]
-        df_test[channel] = df[channel].iloc[int(3*Entries/4):,:]
+        df_train[channel] = df[channel].iloc[:int(Entries/2),:] # select half 0 to entries/2 for the traing
+        df_valid[channel] = df[channel].iloc[int(Entries/2):int(3*Entries/4),:] # select entries/2 to (3/4)*entries for the validate
+        df_test[channel] = df[channel].iloc[int(3*Entries/4):,:] ## select (3/4)*entries to end for the test
         print("total = ", df[channel].shape[0], " df_train = ", df_train[channel].shape[0], " df_valid = ",df_valid[channel].shape[0]," df_test = ",df_test[channel].shape[0])
 
 del df #delete df since information is already split in df_train, df_valid, df_test
@@ -92,20 +96,20 @@ df_signal_train_valid_test_correct_assign = []
 df_signal_train_valid_test_wrong_assign = []
 
 for df in [df_train,df_valid,df_test]:
-	df_temp = pd.concat([df['Tchannel'],df['Tbarchannel']])
-	df_signal_train_valid_test.append(df_temp)
-	df_temp_correct_assign = df_temp.query('Assig==1',inplace = False)
-	df_signal_train_valid_test_correct_assign.append(df_temp_correct_assign)
-	del df_temp_correct_assign
-	df_temp_wrong_assign = df_temp.query('Assig==0',inplace = False)
-	df_signal_train_valid_test_wrong_assign.append(df_temp_wrong_assign)
-	del df_temp_wrong_assign
-	del df_temp
+	df_temp = pd.concat([df['Tchannel'],df['Tbarchannel']])                  # create temp df by adding tchannel and tbarchannel
+	df_signal_train_valid_test.append(df_temp)                # adding temp df to list of df where correcte and inccorrect assigment is not distigued
+	df_temp_correct_assign = df_temp.query('Assig==1',inplace = False)       # saprate out the correct assignment and push to a new temp df
+	df_signal_train_valid_test_correct_assign.append(df_temp_correct_assign) # adding temp df to list of df where only correcte assigment are allowed 
+	del df_temp_correct_assign                                               # delete temp df for correct assignment
+	df_temp_wrong_assign = df_temp.query('Assig==0',inplace = False)         # saprate out the incorrect assignment and push to a new temp df
+	df_signal_train_valid_test_wrong_assign.append(df_temp_wrong_assign)     # adding temp df to list of df where only incorrecte assigment are allowed
+	del df_temp_wrong_assign                                                 # delete temp df for incorrect assignment
+	del df_temp                                                              # delete temp df which created in the begning of the for loop
     
 print "full signal"
 print "train : ", df_signal_train_valid_test[0].shape, "valid : ", df_signal_train_valid_test[1].shape, "test : ", df_signal_train_valid_test[2].shape
 
-del df_signal_train_valid_test # delet the full datafrm for full sample since it aleady split in correct and wrong assignment list
+del df_signal_train_valid_test # delet the full datafrm for full sample since it aleady split in correct and wrong assignment list this was created just to check consitancy
 
 print "only correct assignment signal"
 print "train : ", df_signal_train_valid_test_correct_assign[0].shape, "valid : ", df_signal_train_valid_test_correct_assign[1].shape, "test : ", df_signal_train_valid_test_correct_assign[2].shape
@@ -114,24 +118,28 @@ print "only wrong assignment signal"
 print "train : ", df_signal_train_valid_test_wrong_assign[0].shape, "valid : ", df_signal_train_valid_test_wrong_assign[1].shape, "test : ", df_signal_train_valid_test_wrong_assign[2].shape
 
 
-df_TopBKG_train_valid_test = []  
+df_TopBKG_train_valid_test = []  #define a new list for train test and validation
 for i,df in enumerate([df_train,df_valid,df_test]):
-    df_temp = pd.concat([df['tw_top'],df['tw_antitop'],df['Schannel'],df['ttbar_SemiLeptonic'],df['ttbar_FullyLeptonic'],df_signal_train_valid_test_wrong_assign[i]])
+    df_temp = pd.concat([df['tw_top'],df['tw_antitop'],df['Schannel'],df['ttbar_SemiLeptonic'],df['ttbar_FullyLeptonic'],df_signal_train_valid_test_wrong_assign[i]]) #adding all real top baground and push to a temp df
     df_TopBKG_train_valid_test.append(df_temp)
-    del df_temp
+    del df_temp #delete temp df
 
-del df_signal_train_valid_test_wrong_assign # delete this dataframe since information is alreadyadded in top backgrounds
+del df_signal_train_valid_test_wrong_assign # delete this dataframe since information is alreadyadded to new list of top backgrounds df
 
 print "Top background"
 print "train : ", df_TopBKG_train_valid_test[0].shape, " valid : ", df_TopBKG_train_valid_test[1].shape, " test : ", df_TopBKG_train_valid_test[2].shape
 
 
 
-df_EWKBKG_train_valid_test = []  
+df_EWKBKG_train_valid_test = []  #define a new list for train test and validation
 for i,df in enumerate([df_train,df_valid,df_test]):
-    df_temp = pd.concat([df['WJetsToLNu_0J'],df['WJetsToLNu_1J'],df['WJetsToLNu_2J'],df['DYJets'],df['WWTolnulnu'],df['WZTo2Q2L'],df['ZZTo2Q2L']])
+    df_temp = pd.concat([df['WJetsToLNu_0J'],df['WJetsToLNu_1J'],df['WJetsToLNu_2J'],df['DYJets'],df['WWTolnulnu'],df['WZTo2Q2L'],df['ZZTo2Q2L']]) # adding all EWK bkg and push to a temp df
     df_EWKBKG_train_valid_test.append(df_temp)
-    del df_temp
+    del df_temp # #delete temp df
+
+df_QCD_train_valid_test = []  #define a new list for train test and validation
+for i,df in enumerate([df_train,df_valid,df_test]):
+    df_QCD_train_valid_test.append(df['DataULpreVFP2016'])
 
 del df_train,df_valid,df_test # deleting datadames since the full information already absorbed in signal and backgrounds
 
@@ -140,7 +148,7 @@ print "train : ", df_EWKBKG_train_valid_test[0].shape, " valid : ", df_EWKBKG_tr
 
 #print "df_top_signal_corre_aaign : ",df_top_signal_correct_assign_new
 
-df_signal_train_valid_test_correct_assign[0].to_root('dataframe_saved/preVFP2016_Top_signal_train.root',key='Events')
+df_signal_train_valid_test_correct_assign[0].to_root('dataframe_saved/preVFP2016_Top_signal_train.root',key='Events') # write df in root file
 df_signal_train_valid_test_correct_assign[1].to_root('dataframe_saved/preVFP2016_Top_signal_valid.root',key='Events')
 df_signal_train_valid_test_correct_assign[2].to_root('dataframe_saved/preVFP2016_Top_signal_test.root',key='Events')
 
@@ -158,6 +166,12 @@ df_EWKBKG_train_valid_test[2].to_root('dataframe_saved/preVFP2016_EWK_BKG_test.r
 
 del df_EWKBKG_train_valid_test
 
+
+df_QCD_train_valid_test[0].to_root('dataframe_saved/preVFP2016_QCD_BKG_train.root',key='Events')
+df_QCD_train_valid_test[1].to_root('dataframe_saved/preVFP2016_QCD_BKG_valid.root',key='Events')
+df_QCD_train_valid_test[2].to_root('dataframe_saved/preVFP2016_QCD_BKG_test.root',key='Events')
+
+del df_QCD_train_valid_test
 #KERAS#
 
 """from keras.models import Sequential, Model
