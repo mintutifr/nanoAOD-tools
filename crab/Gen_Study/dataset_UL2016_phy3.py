00904f1a353c80631169ce844cb1eb3705a82bab
Mintu Kumar <mintu.kumar@cern.ch>
2022-07-26 22:18:57 +0530

    update file name and script update

---
+++ crab/Gen_Study/dataset_UL2016_phy3.py
@@ -0,0 +1,4 @@
+Datasets_MC_UL2016 = {
+	'Tbarchannel_wtop0p55'      : ['/ST_t-channel_top_4f_InclusiveDecays_wtop1p45_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/mikumar-Tree_25_Jul22_MCUL2016_Tchannel_wtop1p45-a416af9723181f508c1355aa14d09fc4/USER'],
+
+}
