9617c1ff2e497cddf82bb120e950e1ab37a7114e
Mintu Kumar <mintu.kumar@cern.ch>
2022-08-25 05:15:20 +0200

    newdatasets nanogen phy3

--- crab/Gen_Study/dataset_UL2016.py
+++ crab/Gen_Study/dataset_UL2016.py
@@ -6,6 +6,7 @@ Datasets_Alt_MC_UL2016 ={
         'Tbarchannel_wtop1p15' : '/ST_t-channel_antitop_4f_InclusiveDecays_wtop1p15_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tbarchannel_wtop1p3'  : '/ST_t-channel_antitop_4f_InclusiveDecays_wtop1p3_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tbarchannel_wtop1p45' : '/ST_t-channel_antitop_4f_InclusiveDecays_wtop1p45_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
+	'Tbarchannel_wtop1p31' : '/ST_t-channel_antitop_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1/NANOAODSIM',
 
         'Tchannel_wtop0p55'  : '/ST_t-channel_top_4f_InclusiveDecays_wtop0p55_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tchannel_wtop0p7'   : '/ST_t-channel_top_4f_InclusiveDecays_wtop0p7_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
@@ -14,4 +15,5 @@ Datasets_Alt_MC_UL2016 ={
         'Tchannel_wtop1p15'  : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p15_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tchannel_wtop1p3'   : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p3_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tchannel_wtop1p45'  : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p45_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM' 
+	'Tchannel_wtop1p31'  : '/ST_t-channel_top_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1/NANOAODSIM',
 }
