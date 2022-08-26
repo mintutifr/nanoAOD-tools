387afe05def618da727627b1d406d7467964e59c
Mintu Kumar <mintu.kumar@cern.ch>
2022-08-26 07:39:31 +0200

    dataset update

--- crab/Gen_Study/dataset_UL2016.py
+++ crab/Gen_Study/dataset_UL2016.py
@@ -14,6 +14,6 @@ Datasets_Alt_MC_UL2016 ={
         'Tchannel_wtop1p0'   : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p0_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tchannel_wtop1p15'  : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p15_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
         'Tchannel_wtop1p3'   : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p3_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM',
-        'Tchannel_wtop1p45'  : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p45_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM' 
+        'Tchannel_wtop1p45'  : '/ST_t-channel_top_4f_InclusiveDecays_wtop1p45_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM', 
 	'Tchannel_wtop1p31'  : '/ST_t-channel_top_4f_InclusiveDecays_TuneCP5_13TeV-powheg-madspin-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1/NANOAODSIM',
 }
