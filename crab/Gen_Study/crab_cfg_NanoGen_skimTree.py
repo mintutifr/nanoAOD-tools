01702143064c7bda5746c0d4714d1aa4d422bae6
Mintu Kumar <mintu.kumar@cern.ch>
2022-09-03 21:58:14 +0200

    nGenjet cut nanogen

--- crab/Gen_Study/crab_cfg_NanoGen_skimTree.py
+++ crab/Gen_Study/crab_cfg_NanoGen_skimTree.py
@@ -5,7 +5,7 @@ config = Configuration()
 
 config.section_("General")
 
-config.General.requestName = 'Tbarchannel_wtop0p55_Tree_UL2016'
+config.General.requestName = 'Tbarchannel_wtop1p45_Tree_UL2016'
 
 config.General.transferLogs=True
 config.section_("JobType")
@@ -17,17 +17,17 @@ config.JobType.inputFiles = ['crab_script_NanoGen_skimtree.py','../../scripts/ha
 config.JobType.sendPythonFolder=True
 config.section_("Data")
 
-config.Data.inputDataset = '/ST_t-channel_antitop_4f_InclusiveDecays_wtop0p55_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM'
+config.Data.inputDataset = '/ST_t-channel_antitop_4f_InclusiveDecays_wtop1p45_TuneCP5_fixWidth_13TeV-powheg-madspin-pythia8/RunIISummer20UL16wmLHENanoGEN-106X_mcRun2_asymptotic_v13-v1/NANOAODSIM'
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 
-config.Data.outLFNDirBase = '/store/user/mikumar/RUN2_UL/Tree_crab/SIXTEEN/Mc_NANOGEN/Tbarchannel_wtop0p55'
+config.Data.outLFNDirBase = '/store/user/mikumar/RUN2_UL/Tree_crab/SIXTEEN/Mc_NANOGEN_v7/Tbarchannel_wtop1p45'
 
 config.Data.publication = True
 
-config.Data.outputDatasetTag = 'Tree_26_Jul22_MCUL2016_Tbarchannel_wtop0p55_v2'
+config.Data.outputDatasetTag = 'Tree_03_Sep22_MCUL2016_Tbarchannel_wtop1p45_v7'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_IN_TIFR'
