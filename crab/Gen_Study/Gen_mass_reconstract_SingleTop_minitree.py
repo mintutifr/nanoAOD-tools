279a5364f314b8c531e972c13f92c4ea1453cc25
Mintu Kumar <mintu.kumar@cern.ch>
2024-09-12 15:05:32 +0530

    starts ttbar gen module

--- crab/Gen_Study/Gen_mass_reconstract_SingleTop_minitree.py
+++ crab/Gen_Study/Gen_mass_reconstract_SingleTop_minitree.py
@@ -10,15 +10,11 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from Gen_mass_functions import *
 class NanoGenModule_minitree(Module):
     def __init__(self,datayear):
-	self.writeHistFile=True
-	self.datayear = datayear
-    #def beginJob(self,histFile=None,histDirName=None):
-	#Module.beginJob(self,histFile,histDirName)
-	#self.pt_hist = ROOT.TH1D("pt_hist","pt_hist",20,0.0,400)
-	#self.eta_hist = ROOT.TH1D("eta_hist","eta_hist",20,0.0,5.0)
+        self.writeHistFile=True
+        self.datayear = datayear
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-	pass
+        pass
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
     def analyze(self, event):
