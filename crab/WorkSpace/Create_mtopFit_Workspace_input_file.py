63cf486c3d11fcf9f8e75a6f20a01f72338e24ad
Mintu Kumar <mintu.kumar@cea.fr>
2025-02-04 11:47:43 +0100

    add Jes sys hist to file

--- crab/WorkSpace/Create_mtopFit_Workspace_input_file.py
+++ crab/WorkSpace/Create_mtopFit_Workspace_input_file.py
@@ -137,6 +137,127 @@ def get_Weight_sys_EWK(lep,year,Variable,channels_weight_sys,MCcut,DNNCut,hist_s
 
     return EWK_weight_sys
 
+def get_Weight_sys_top_JES(lep,year,Variable,channels_weight_sys,MCcut,DNNcut,hist_sys_name,File_with_mtwMassFit_weight_Iso,Fpaths_DNN_apply,top_sig_cons,top_bkg_cons,Fpaths_sys_samples="None"):
+    QCDcut=""
+    Datacut=""
+    if(Fpaths_sys_samples==None):
+        if(Variable=="TMath::Log(topMass)"): Variable="lntopMass"
+        hists_corr_weight_sys,hists_wron_weight_sys =  get_histogram_with_DNN_cut(
+            lep=lep,
+            year=year,
+            Variable=Variable,
+            channels = channels_weight_sys,
+            MCcut = MCcut ,
+            QCDcut = QCDcut, 
+            Datacut = Datacut, 
+            DNNcut = DNNcut,
+            Filepaths_with_QCDWeight = File_with_mtwMassFit_weight_Iso,
+            Fpaths_DNN_score = Fpaths_DNN_apply,
+            Fpaths_sys_samples = Fpaths_sys_samples
+        )
+        if(Variable=="lntopMass"):   Variable="TMath::Log(topMass)"
+    else:
+        hists_corr_weight_sys,hists_wron_weight_sys =  get_histogram_with_DNN_cut(
+            lep=lep,
+            year=year,
+            Variable=Variable,
+            channels = channels_weight_sys,
+            MCcut = MCcut ,
+            QCDcut = QCDcut, 
+            Datacut = Datacut, 
+            DNNcut = DNNcut,
+            Filepaths_with_QCDWeight = File_with_mtwMassFit_weight_Iso,
+            Fpaths_DNN_score = Fpaths_DNN_apply,
+            Fpaths_sys_samples = Fpaths_sys_samples
+        )
+    hist_corr_assig_weight_sys = {}
+    hist_wron_assig_weight_sys = {}
+    for channel_no,channel in enumerate(channels_weight_sys):
+        hist_corr_assig_weight_sys[channel] = hists_corr_weight_sys[channel_no].Clone()
+        hist_wron_assig_weight_sys[channel] = hists_wron_weight_sys[channel_no].Clone()
+    del hists_corr_weight_sys
+    del hists_wron_weight_sys
+
+    top_sig_weight_sys = hist_corr_assig_weight_sys["Tchannel"].Clone(); top_sig_weight_sys.Add(hist_corr_assig_weight_sys["Tbarchannel"]);
+
+    top_bkg_weight_sys = hist_wron_assig_weight_sys["Tchannel"].Clone(); top_bkg_weight_sys.Add(hist_wron_assig_weight_sys["Tbarchannel"]);
+
+    propagate_rate_uncertainity(top_sig_weight_sys, top_sig_cons)
+    propagate_rate_uncertainity(top_bkg_weight_sys, top_sig_cons)
+
+    top_bkg_weight_sys_corr = hist_corr_assig_weight_sys['tw_top'].Clone();
+    top_bkg_weight_sys_wron = hist_wron_assig_weight_sys['tw_top'].Clone();
+    for channel in ['tw_antitop', 'Schannel','ttbar_SemiLeptonic','ttbar_FullyLeptonic']:
+        top_bkg_weight_sys_corr.Add(hist_corr_assig_weight_sys[channel])
+        top_bkg_weight_sys_wron.Add(hist_wron_assig_weight_sys[channel])
+
+    propagate_rate_uncertainity(top_bkg_weight_sys_corr, top_bkg_cons)
+    propagate_rate_uncertainity(top_bkg_weight_sys_wron, top_bkg_cons)
+
+    top_sig_weight_sys.Add(top_bkg_weight_sys_corr)
+    top_bkg_weight_sys.Add(top_bkg_weight_sys_wron)
+
+    top_sig_weight_sys.Print()
+    top_bkg_weight_sys.Print()
+
+    return top_sig_weight_sys,top_bkg_weight_sys
+
+def get_Weight_sys_EWK_JES(lep,year,Variable,channels_weight_sys,MCcut,DNNcut,hist_sys_name,File_with_mtwMassFit_weight_Iso,Fpaths_DNN_apply,EWK_cons,Fpaths_sys_samples="None"):
+
+    QCDcut=""
+    Datacut=""
+    if(Fpaths_sys_samples==None):
+        if(Variable=="TMath::Log(topMass)"): Variable="lntopMass"
+        hists_corr_weight_sys,hists_wron_weight_sys =  get_histogram_with_DNN_cut(
+            lpe=lep,
+            year=year,
+            Variable=Variable,
+            channels=channels_weight_sys, 
+            MCcut=MCcut ,
+            QCDcut=QCDcut, 
+            Datacut=Datacut , 
+            DNNcut=DNNcut ,
+            Filepaths_with_QCDWeight = File_with_mtwMassFit_weight_Iso,
+            Fpaths_DNN_score = Fpaths_DNN_apply,
+            Fpaths_sys_samples = Fpaths_sys_samples
+        )
+        if(Variable=="lntopMass"):   Variable="TMath::Log(topMass)"
+    else:
+        hists_corr_weight_sys,hists_wron_weight_sys =  get_histogram_with_DNN_cut(
+            lep=lep,
+            year=year,
+            Variable=Variable,
+            channels=channels_weight_sys, 
+            MCcut=MCcut ,
+            QCDcut=QCDcut, 
+            Datacut=Datacut , 
+            DNNcut=DNNcut ,
+            Filepaths_with_QCDWeight = File_with_mtwMassFit_weight_Iso,
+            Fpaths_DNN_score = Fpaths_DNN_apply,
+            Fpaths_sys_samples = Fpaths_sys_samples
+        )
+
+
+    hist_corr_assig_weight_sys = {}
+    hist_wron_assig_weight_sys = {}
+    for channel_no,channel in enumerate(channels_weight_sys):
+        hist_corr_assig_weight_sys[channel] = hists_corr_weight_sys[channel_no].Clone()
+        hist_wron_assig_weight_sys[channel] = hists_wron_weight_sys[channel_no].Clone()
+    del hists_corr_weight_sys
+    del hists_wron_weight_sys
+
+    EWK_weight_sys = hist_corr_assig_weight_sys['WJetsToLNu_0J'].Clone()
+    EWK_weight_sys.Add(hist_wron_assig_weight_sys['WJetsToLNu_0J'])
+    for channel in ['WJetsToLNu_1J', 'WJetsToLNu_2J', 'DYJetsToLL', 'WWTo2L2Nu', 'WZTo2Q2L', 'ZZTo2Q2L']:
+        EWK_weight_sys.Add(hist_corr_assig_weight_sys[channel])
+        EWK_weight_sys.Add(hist_wron_assig_weight_sys[channel])
+
+    propagate_rate_uncertainity(EWK_weight_sys, EWK_cons)
+    EWK_weight_sys.Print()
+
+    return EWK_weight_sys
+
+
 def Create_Workspace_input_file(lep="mu",year="UL2017",Variable="lntopMass"):
 
     if(lep=="mu"):
@@ -665,34 +786,63 @@ def Create_Workspace_input_file(lep="mu",year="UL2017",Variable="lntopMass"):
                 del EWK_hists_syst
         del sys_bWeight,sys_variation
     if(allsys or JES_JERSys):
-        sys_JES = ["jesAbsoluteStat"]
+        sys_JES = ['jesAbsoluteStat', 'jesAbsoluteMPFBias', 'jesFragmentation', 'jesSinglePionECAL', 'jesSinglePionHCAL',
+                  'jesTimePtEta', 'jesRelativeJEREC1', 'jesRelativeJEREC2', 'jesRelativeJERHF', 'jesRelativePtBB',
+                  'jesRelativePtEC1', 'jesRelativePtEC2', 'jesRelativePtHF', 'jesRelativeBal', 'jesRelativeSample',
+                  'jesRelativeFSR', 'jesRelativeStatEC', 'jesRelativeStatHF', 'jesPileUpDataMC', 'jesPileUpPtRef',
+                  'jesPileUpPtBB', 'jesPileUpPtEC1', 'jesPileUpPtEC2', 'jesPileUpPtHF']
+        sys_JES = ['jesAbsoluteStat','jesAbsoluteMPFBias']
         sys_variation = ["Up","Down"]
         channel_JES = ["Tchannel"]
-        Fpaths_sys_samples = {}
-        for channel in channel_JES:
-            Fpaths_sys_samples[channel] = "/feynman/home/dphp/mk277705/work/RUN2_UL/Minitree_corr_bweight/JER_JES_Trees/JERJEStree_"+year+"_"+channel+"_2J1T1_"+lep+".root"
+        Fpaths_sys_samples_top,Fpaths_sys_samples_EWK = {},{}
+        for channel in channels_top_only:
+            Fpaths_sys_samples_top[channel] = "/feynman/home/dphp/mk277705/work/RUN2_UL/Minitree_corr_bweight/JER_JES_Trees/JERJEStree_"+year+"_"+channel+"_2J1T1_"+lep+".root"
+        for channel in channels_EWK_only:
+            Fpaths_sys_samples_EWK[channel] = "/feynman/home/dphp/mk277705/work/RUN2_UL/Minitree_corr_bweight/JER_JES_Trees/JERJEStree_"+year+"_"+channel+"_2J1T1_"+lep+".root"
             
         for sys in sys_JES:
             for variation_no,variation in enumerate(sys_variation):
                 print("\n #################  ", sys+variation, "############## \n")
                 print(f"{MCcut = }")
                 if(Variable=="TMath::Log(topMass)"): Variable="lntopMass_"+sys+variation
-                hists_corr,hists_wron =  get_histogram_with_DNN_cut(lep,year,Variable,channel_JES, MCcut ,QCDcut, Datacut , DNNCut ,File_with_mtwMassFit_weight_Iso,Fpaths_DNN_apply,Fpaths_sys_samples = Fpaths_sys_samples)
+                topSig_hists_JES_syst,topBkg_hists_JES_syst = get_Weight_sys_top_JES(
+                    lep=lep,
+                    year=year,
+                    Variable=Variable,
+                    channels_weight_sys=channels_top_only,
+                    MCcut = MCcut,
+                    DNNcut = DNNCut,
+                    hist_sys_name="_"+lep+sys,
+                    File_with_mtwMassFit_weight_Iso=File_with_mtwMassFit_weight_Iso,
+                    Fpaths_DNN_apply=Fpaths_DNN_apply,top_sig_cons=top_sig_cons,
+                    top_bkg_cons=top_bkg_cons,
+                    Fpaths_sys_samples = Fpaths_sys_samples_top
+                )
+                topSig_hists_JES_syst.SetName("top_sig_1725"+tag+gt_or_lt_tag+sys+variation)
+                topBkg_hists_JES_syst.SetName("top_bkg_1725"+tag+gt_or_lt_tag+sys+variation)
+                hist_to_return.append(topSig_hists_JES_syst.Clone())
+                hist_to_return.append(topBkg_hists_JES_syst.Clone())
+                del topSig_hists_JES_syst,topBkg_hists_JES_syst
+                EWK_hists_syst_JES = get_Weight_sys_EWK_JES(
+                    lep=lep,
+                    year=year,
+                    Variable=Variable,
+                    channels_weight_sys=channels_EWK_only,
+                    MCcut = MCcut,
+                    DNNcut = DNNCut, 
+                    hist_sys_name="_"+lep+sys,
+                    File_with_mtwMassFit_weight_Iso=File_with_mtwMassFit_weight_Iso,
+                    Fpaths_DNN_apply=Fpaths_DNN_apply,
+                    EWK_cons=EWK_bkg_cons,
+                    Fpaths_sys_samples = Fpaths_sys_samples_EWK
+                )
+                sys_EWK_Integral = EWK_hists_syst_JES.Integral()
+                EWK_hists_syst_JES = Control_EWK_hist.Clone()
+                EWK_hists_syst_JES.Scale(sys_EWK_Integral/control_region_EWK_Integral)
+                EWK_hists_syst_JES.SetName("EWK_bkg"+tag+gt_or_lt_tag+sys)
+                hist_to_return.append(EWK_hists_syst_JES.Clone())
+                del EWK_hists_syst_JES
                 if("lntopMass_" in Variable): Variable="TMath::Log(topMass)"
-
-                #topSig_hists_syst,topBkg_hists_syst = get_histogram_with_DNN_cut(lep=lep,year=year,Variable=Variable,channels_weight_sys=channel_JES,MCcut = MCcut,DNNCut = DNNCut,hist_sys_name="_"+lep+sys,File_with_mtwMassFit_weight_Iso=File_with_mtwMassFit_weight_Iso,Fpaths_DNN_apply=Fpaths_DNN_apply,top_sig_cons=top_sig_cons,top_bkg_cons=top_bkg_cons)
-                # topSig_hists_syst.SetName("top_sig_1725"+tag+gt_or_lt_tag+sys+variation)
-                # topBkg_hists_syst.SetName("top_bkg_1725"+tag+gt_or_lt_tag+sys+variation)
-                # hist_to_return.append(topSig_hists_syst.Clone())
-                # hist_to_return.append(topBkg_hists_syst.Clone())
-                # del topSig_hists_syst,topBkg_hists_syst
-                # EWK_hists_syst = get_Weight_sys_EWK(lep=lep,year=year,Variable=Variable,channels_weight_sys=channels_EWK_only,MCcut = MCcut_sys_bWeight,DNNCut = DNNCut, hist_sys_name="_"+lep+sys,File_with_mtwMassFit_weight_Iso=File_with_mtwMassFit_weight_Iso,Fpaths_DNN_apply=Fpaths_DNN_apply,EWK_cons=EWK_bkg_cons)
-                # sys_EWK_Integral = EWK_hists_syst.Integral()
-                # EWK_hists_syst = Control_EWK_hist.Clone()
-                # EWK_hists_syst.Scale(sys_EWK_Integral/control_region_EWK_Integral)
-                # EWK_hists_syst.SetName("EWK_bkg"+tag+gt_or_lt_tag+sys)
-                # hist_to_return.append(EWK_hists_syst.Clone())
-                # del EWK_hists_syst
         del sys_JES,sys_variation
 
     if(allsys or colorReconect):
--- crab/WorkSpace/Get_Histogram_after_DNN_cuts.py
+++ crab/WorkSpace/Get_Histogram_after_DNN_cuts.py
@@ -59,7 +59,7 @@ def get_histogram_with_DNN_cut(
     MCcut_corr_Assig = MCcut+DNNcut+"*(bJetpartonFlavour*"+lepton+"Charge==5)"
     MCcut_wron_Assig = MCcut+DNNcut+"*(bJetpartonFlavour*"+lepton+"Charge!=5)"
     for channel in channels:
-            print(channel, "  ", Filepaths_with_QCDWeight[channel])
+            print("\n",channel, "  ", Filepaths_with_QCDWeight[channel])
             print(channel, "  ", Fpaths_DNN_score[channel])
             if Fpaths_sys_samples is not None and channel in Fpaths_sys_samples: 
                  print(channel, "  ", Fpaths_sys_samples[channel])
@@ -74,7 +74,7 @@ def get_histogram_with_DNN_cut(
                  intree.AddFriend ("Events",Fpaths_sys_samples[channel])
         #intree[-1].Print()
             rt.gROOT.cd()
-            print("Projecting :",Variable," with cut : ",MCcut_corr_Assig)
+            print("\nProjecting :",Variable," with cut : ",MCcut_corr_Assig)
             intree.Project('histo_corr', Variable, MCcut_corr_Assig)
             intree.Project('histo_wron', Variable, MCcut_wron_Assig)
             # Define the dataset
