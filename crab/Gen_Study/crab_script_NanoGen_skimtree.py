482efaa1d199b369d24581db2e7166f30311d24e
mikumar <mintu.kumar@cern.ch>
2026-01-09 17:52:02 +0100

    update the Gen study and top last copy

--- crab/Gen_Study/crab_script_NanoGen_skimtree.py
+++ crab/Gen_Study/crab_script_NanoGen_skimtree.py
@@ -6,18 +6,18 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 #import main module
 from Gen_mass_reconstract_SingleTop import *
-from Gen_mass_reconstract_ttbar import *
+#from Gen_mass_reconstract_ttbar import *
 
 #treecut = "Entry$<1000"
-treecut = "nGenDressedLepton>0 && nGenJet>0 && Entry$<2"
+treecut = "nGenDressedLepton>0 && nGenJet>0" # && Entry$<200"
 #nJet>0 && Jet_pt>20 && (Sum$(Muon_pt>20)>0 || Sum$(Electron_pt>30)>0)" #&& Entry$<10000"#(nMuon>0 || nElectron>0)" # && (Muon_pt>5 || Electron_pt>10) (nMuon>0 || nElectron>0)"
-inputFiles=["5FB0F88E-E721-3C44-B35E-69AD33105425.root"]
+#inputFiles=["5FB0F88E-E721-3C44-B35E-69AD33105425.root"]
 #inputFiles=[ "/home/mikumar/t3store3/workarea/Nanoaod_tools/CMSSW_10_2_28/src/PhysicsTools/NanoAODTools/crab/Gen_Study/NANOGEN_file_wtop1p0.root"] 
-	
+#inputFiles=[ "72DF6C8D-6A14-C745-A924-8CBFB1C3592A.root"]
 p=PostProcessor( ".",
-        inputFiles,
+        inputFiles(),
         treecut,
-	    modules=[NanoGenConstr_ttbar_UL2016()],
+	modules=[NanoGenConstr_UL2016_Alt_mass()],
         outputbranchsel="clean_All_keep_GenPart.txt",
         provenance=True,
         fwkJobReport=True,
